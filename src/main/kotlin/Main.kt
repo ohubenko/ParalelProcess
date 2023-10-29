0238689b348d943c6682de09f86966b0bfd29084
Oleksandr Hubenko <o.hubenko@spd-ukraine.com>
2023-10-29 12:51:35 +0200

    Прибрали 0 з масиву і це РОБОЧИЙ варіант

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,45 +1,44 @@
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import kotlinx.coroutines.channels.Channel
+import java.util.concurrent.atomic.AtomicInteger
 
 fun main() = runBlocking {
-    val inputArray = intArrayOf(1, 2, 3, 4, 5, 6)
+    var inputList = mutableListOf(1, 2, 3, 4, 5, 6)
     val numberOfProcessors = Runtime.getRuntime().availableProcessors()
-    val tasksChannel = Channel<Int>(Channel.UNLIMITED)
-    val doneChannel = Channel<Unit>(Channel.UNLIMITED)
+    val tasksChannel = Channel<Int?>(Channel.UNLIMITED)
+    val doneCounter = AtomicInteger(0)
     val mutex = Mutex()
-
-    val activeElements = MutableList(inputArray.size) { i -> i }
 
     val workerJobs = List(numberOfProcessors) {
         launch {
             for (index in tasksChannel) {
-                val oppositeIndex = activeElements.size - 1 - index
-                mutex.withLock {
-                    inputArray[activeElements[index]] += inputArray[activeElements[oppositeIndex]]
-                    inputArray[activeElements[oppositeIndex]] = 0
+                index?.let {
+                    mutex.withLock {
+                        val oppositeIndex = inputList.size - 1 - it
+                        inputList[it] += inputList[oppositeIndex]
+                        inputList.removeAt(oppositeIndex)
+                    }
+                    doneCounter.incrementAndGet()
                 }
-                doneChannel.send(Unit)
             }
         }
     }
 
-    while (activeElements.size > 1) {
-        val operations = activeElements.size / 2
-        for (i in 0 until operations) {
+    while (inputList.size > 1) {
+        for (i in 0 until inputList.size / 2) {
             tasksChannel.send(i)
         }
 
-        // Очікуємо завершення всіх завдань цієї хвилі
-        repeat(operations) {
-            doneChannel.receive()
+        while (doneCounter.get() < inputList.size / 2) {
+            delay(10)
         }
-
-        activeElements.subList(operations, activeElements.size).clear()
+        doneCounter.set(0)
     }
 
     tasksChannel.close()
     workerJobs.forEach { it.join() }
-    println(inputArray.toList())
+
+    println(inputList)
 }
