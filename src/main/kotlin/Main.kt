a719ac5ba87ab2d3370936ebb660a63bb83e6ef6
Oleksandr Hubenko <o.hubenko@spd-ukraine.com>
2023-10-29 13:13:40 +0200

    Зробив правильну синхронізацію

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,14 +4,12 @@ import kotlinx.coroutines.sync.withLock
 
 fun main() = runBlocking {
     val inputList = mutableListOf(1, 2, 3, 4, 5, 6)
-    val numberOfProcessors = Runtime.getRuntime().availableProcessors()
     val mutex = Mutex()
-
     // Основна логіка обчислення суми парних елементів
-    suspend fun processPairSum(index: Int) {
+    suspend fun processPairSum(index: Int, oppositeIndex: Int) {
+        val sum = inputList[index] + inputList[oppositeIndex]
         mutex.withLock {
-            val oppositeIndex = inputList.lastIndex - index
-            inputList[index] += inputList[oppositeIndex]
+            inputList[index] = sum
             inputList.removeAt(oppositeIndex)
         }
     }
@@ -20,13 +18,13 @@ fun main() = runBlocking {
         // Ініціалізація корутин для кожної пари
         val jobs = mutableListOf<Job>()
         val halfSize = inputList.size / 2
-        for (i in 0 until halfSize) {
-            jobs.add(launch { processPairSum(i) })
+        for (i in 0..<halfSize) {
+            val oppositeIndex = inputList.lastIndex - i
+            jobs.add(launch { processPairSum(i, oppositeIndex) })
         }
-
         // Очікування завершення всіх корутин
         jobs.forEach { it.join() }
+        println(inputList) // Вивід результату
     }
-
     println(inputList[0]) // Вивід результату
 }
