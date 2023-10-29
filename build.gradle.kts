39a93876ebb46723a678338ca4e86006c140f734
Oleksandr Hubenko <o.hubenko@spd-ukraine.com>
2023-10-29 12:29:31 +0200

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.0")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,51 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.channels.Channel
+
+fun main() = runBlocking {
+    val inputArray = intArrayOf(1, 2, 3, 4, 5, 6)
+    val numberOfProcessors = Runtime.getRuntime().availableProcessors()
+    val tasksChannel = Channel<Int?>(Channel.UNLIMITED) // Null використовується як позначка завершення
+    val mutex = Mutex()
+    val doneChannel = Channel<Unit>(Channel.UNLIMITED)
+
+    val workerJobs = List(numberOfProcessors) {
+        launch {
+            for (index in tasksChannel) {
+                if (index == null) continue
+
+                val oppositeIndex = inputArray.size - 1 - index
+                mutex.withLock {
+                    inputArray[index] += inputArray[oppositeIndex]
+                    inputArray[oppositeIndex] = 0
+                }
+                doneChannel.send(Unit)
+            }
+        }
+    }
+
+    var activeLength = inputArray.size
+    while (activeLength > 1) {
+        val halfLength = activeLength / 2
+        for (i in 0 until halfLength) {
+            tasksChannel.send(i)
+        }
+
+        // Очікуємо завершення всіх завдань цієї хвилі
+        repeat(halfLength) {
+            doneChannel.receive()
+        }
+
+        activeLength = halfLength
+    }
+
+    // Сигнал до робочих потоків про завершення роботи
+    repeat(numberOfProcessors) {
+        tasksChannel.send(null)
+    }
+
+    tasksChannel.close()
+    workerJobs.forEach { it.join() }
+    println(inputArray[0])
+}
