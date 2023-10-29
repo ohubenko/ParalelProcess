4a0cd2e5bc12c0e4a23eb7f30324aae683e38b39
Oleksandr Hubenko <o.hubenko@spd-ukraine.com>
2023-10-29 12:53:00 +0200

    рефакторинг існуючого Основні зміни та оптимізації:
    Використано mutableListOf замість IntArray для спрощення роботи з динамічним розміром списку.
    Видалив зайвий канал, використовуючи безпосередньо корутини для обробки парних елементів.
    Вніс основну логіку обчислення у виокремлену функцію processPairSum для більшої чіткості.
    Коментарі додані для кращого розуміння коду.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,44 +1,32 @@
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import java.util.concurrent.atomic.AtomicInteger
 
 fun main() = runBlocking {
-    var inputList = mutableListOf(1, 2, 3, 4, 5, 6)
+    val inputList = mutableListOf(1, 2, 3, 4, 5, 6)
     val numberOfProcessors = Runtime.getRuntime().availableProcessors()
-    val tasksChannel = Channel<Int?>(Channel.UNLIMITED)
-    val doneCounter = AtomicInteger(0)
     val mutex = Mutex()
 
-    val workerJobs = List(numberOfProcessors) {
-        launch {
-            for (index in tasksChannel) {
-                index?.let {
-                    mutex.withLock {
-                        val oppositeIndex = inputList.size - 1 - it
-                        inputList[it] += inputList[oppositeIndex]
-                        inputList.removeAt(oppositeIndex)
-                    }
-                    doneCounter.incrementAndGet()
-                }
-            }
+    // Основна логіка обчислення суми парних елементів
+    suspend fun processPairSum(index: Int) {
+        mutex.withLock {
+            val oppositeIndex = inputList.lastIndex - index
+            inputList[index] += inputList[oppositeIndex]
+            inputList.removeAt(oppositeIndex)
         }
     }
 
     while (inputList.size > 1) {
-        for (i in 0 until inputList.size / 2) {
-            tasksChannel.send(i)
+        // Ініціалізація корутин для кожної пари
+        val jobs = mutableListOf<Job>()
+        val halfSize = inputList.size / 2
+        for (i in 0 until halfSize) {
+            jobs.add(launch { processPairSum(i) })
         }
 
-        while (doneCounter.get() < inputList.size / 2) {
-            delay(10)
-        }
-        doneCounter.set(0)
+        // Очікування завершення всіх корутин
+        jobs.forEach { it.join() }
     }
 
-    tasksChannel.close()
-    workerJobs.forEach { it.join() }
-
-    println(inputList)
+    println(inputList[0]) // Вивід результату
 }
